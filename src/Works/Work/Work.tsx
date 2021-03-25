3ecca32965fd1c60be37c43f60820b39dc7d0f2b
vsmaliakou <vsmaliakou@yandex.by>
2021-03-25 09:33:38 +0300

    25.03.2021

--- src/Works/Work/Work.tsx
+++ src/Works/Work/Work.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import style from './Work.module.css'
 
+
 type WorkType = {
     title: string
     description: string
+    src: string
 }
 
 const Work: React.FC<WorkType> = (props) => {
     return (
         <div className={style.container}>
             <div className={style.imgBlock}>
-                <img/>
+                <img src={props.src}/>
+                <button>Look</button>
             </div>
             <h2>{props.title}</h2>
             <span>{props.description}</span>
--- src/Works/Works.tsx
+++ src/Works/Works.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import style from './Works.module.css'
 import styleContainer from "../Common/Styles/Container.module.css";
 import Work from "./Work/Work";
+import work1 from '../assets/image/work1.jpeg'
+import work2 from '../assets/image/work2.gif'
 
 const Works = () => {
     return (
@@ -9,8 +11,8 @@ const Works = () => {
             <div className={`${styleContainer.container} ${style.worksContainer}`}>
                 <h2 className={style.title}>Works</h2>
                 <div className={style.container}>
-                    <Work title="the name of the project" description="short description"/>
-                    <Work title="the name of the project" description="short description"/>
+                    <Work title="the name of the project" description="short description" src={work1}/>
+                    <Work title="the name of the project" description="short description" src={work2}/>
                 </div>
             </div>
         </div>
