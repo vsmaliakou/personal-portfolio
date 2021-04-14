800ea483d18c904a39a47dffefdccdde673860ef
vsmaliakou <vsmaliakou@yandex.by>
2021-04-14 17:04:35 +0300

    14.04.2021 - sass

--- src/Works/Work/Work.tsx
+++ src/Works/Work/Work.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import style from './Work.module.css'
+import style from './Work.module.scss'
 
 type WorkType = {
     title: string
     description: string
+    style: {backgroundImage: string}
 }
 
 const Work: React.FC<WorkType> = (props) => {
     return (
         <div className={style.work}>
-            <div className={style.image}>
-                <a className={style.view}>Смотреть</a>
+            <div className={style.image} style={props.style}>
+                <div className={style.opacityBlock}>
+                    <h3 className={style.workTitle}>{props.title}</h3>
+                    <span className={style.description}>{props.description}</span>
+                </div>
             </div>
-            <h3>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
         </div>
     )
 }
--- src/Works/Works.tsx
+++ src/Works/Works.tsx
@@ -1,17 +1,45 @@
 import React from 'react';
-import style from './Works.module.css'
+import style from './Works.module.scss'
 import styleContainer from "../Common/Styles/Container.module.css";
 import Work from "./Work/Work";
 import Title from "../Common/Components/title/Title";
+import socialNetworkImage from '../assets/images/socialnetwork.png'
+import todolistImage from '../assets/images/todolist.jpg'
+import counterImage from '../assets/images/counter.jpg'
+
 
 const Works = () => {
+
+    const socialNetwork = {
+        backgroundImage: `url(${socialNetworkImage})`
+    }
+    const todolist = {
+        backgroundImage: `url(${todolistImage})`
+    }
+    const counter = {
+        backgroundImage: `url(${counterImage})`
+    }
+
     return (
         <div className={style.worksBlock}>
             <div className={`${styleContainer.container} ${style.worksContainer}`}>
                 <Title text="MY WORKS"/>
                 <div className={style.container}>
-                    <Work title="the name of the project" description="short description" />
-                    <Work title="the name of the project" description="short description" />
+                    <Work
+                        title="Social network"
+                        description="short description 1"
+                        style={socialNetwork}
+                    />
+                    <Work
+                        title="Todolist"
+                        description="short description 2"
+                        style={todolist}
+                    />
+                    <Work
+                        title="Counter"
+                        description="short description 3"
+                        style={counter}
+                    />
                 </div>
             </div>
         </div>
